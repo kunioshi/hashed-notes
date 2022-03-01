3dca539d9c05ac68e541f2516a1cd1531f376941
Filipe Kunioshi <filipekunioshi@hotmail.com>
2022-03-01 11:13:49 -0300

    Implement DB option and add parseTime

--- api/services/db.go
+++ api/services/db.go
@@ -14,13 +14,15 @@ var Db *gorm.DB = initDb()
 // Initialize the database connection. It creates `*gorm.DB` and saves it in global `Db`.
 func initDb() *gorm.DB {
 	env := config.GetEnv()
+	opt := "parseTime=true"
 	srcName := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s",
+		"%s:%s@tcp(%s:%s)/%s?%s",
 		env["DB_USER"],
 		env["DB_PASS"],
 		env["DB_HOST"],
 		env["DB_PORT"],
 		env["DB_NAME"],
+		opt,
 	)
 
 	// TODO: Accept other DB drivers other than MySQL
