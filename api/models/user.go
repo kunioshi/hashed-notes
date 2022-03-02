bcedbd8c85fd803c5b30b0d2b47fb98856fccf3a
Filipe Kunioshi <filipekunioshi@hotmail.com>
2022-03-02 09:38:56 -0300

    Change User fields, remove confirm to separate

--- api/models/user.go
+++ api/models/user.go
@@ -6,18 +6,16 @@ import (
 
 type User struct {
 	gorm.Model
-	Username     string `form:"username" json:"username" binding:"required" gorm:"unique"`
-	Password     string `form:"password" json:"-" binding:"required"`
-	Email        string `form:"email" json:"email" binding:"required" gorm:"unique"`
-	Confirmation string `form:"confirmation" json:"-"`
+	Username string `form:"username" json:"username" gorm:"unique"`
+	Password string `form:"password" json:"password"`
+	Email    string `form:"email" json:"email" binding:"email" gorm:"unique"`
 }
 
-func NewUser(u string, p string, e string, c string) *User {
+func NewUser(u string, p string, e string) *User {
 	nu := new(User)
 	nu.Username = u
 	nu.Password = p
 	nu.Email = e
-	nu.Confirmation = c
 
 	return nu
 }
